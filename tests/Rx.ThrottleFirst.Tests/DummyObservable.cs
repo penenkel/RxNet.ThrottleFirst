d75731ab219e04d74e7746fc56bd1895abf43388
Peter Enenkel <peter.enenkel@gmail.com>
2022-05-29 23:54:09 +0200

    Add project files.

---
+++ tests/Rx.ThrottleFirst.Tests/DummyObservable.cs
@@ -0,0 +1,15 @@
+﻿namespace Rx.ThrottleFirst.Tests;
+
+internal class DummyObservable<T> : IObservable<T>
+{
+    public static readonly DummyObservable<T> Instance = new();
+
+    private DummyObservable()
+    {
+    }
+
+    public IDisposable Subscribe(IObserver<T> observer)
+    {
+        throw new NotImplementedException();
+    }
+}
