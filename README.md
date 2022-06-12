5a5b25873b92de01e7f207282fec06082a7d2b75
Peter Enenkel <peter.enenkel@gmail.com>
2022-06-12 22:28:18 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,32 +1,60 @@
 # Purpose
 
-Provides the `ThrottleFirst` filtering operator for RxNET.
+Provides the `ThrottleFirst` filtering operator for Rx.Net.
 
 ![](https://reactivex.io/documentation/operators/images/throttleFirst.png){height=250}
 
-Given a source Observable and a duration, it emits the _first_ item from the source and then skips subsequent items until the given duration has elapsed. The next source item after that is emitted again and the duration starts anew.
+Given a source Observable and a suppression period TimeSpan: Emit the _first_ item from the source. Then skip subsequent items until the given period has elapsed. Then emit the next source item and start the suppression period anew.
 ```
-IObservable<T> ThrottleFirst<T>(this IObservable<T> source, TimeSpan duration)
+IObservable<T> ThrottleFirst<T>(this IObservable<T> source, TimeSpan suppressionPeriod)
 ```
 
-Ther is also a version where the duration is provided by a selector function. That function accepts the source item as input and returns an Observable, which in turn signals the end of the duration when it emits.
+There is also a version where the suppression period is provided by a selector function. That function accepts the emitted source item as input and returns an Observable. Which in turn signals the end of the suppression period when it emits.
 ```
-IObservable<T> ThrottleFirst<T, TThrottle>(this IObservable<T> source, Func<T, IObservable<TThrottle>> durationSelector)
+IObservable<T> ThrottleFirst<T, TThrottle>(this IObservable<T> source, Func<T, IObservable<TThrottle>> suppressionPeriodSelector)
 ```
 
 # Motivation
 
-There is significant disparity over the various reactive implementations what the various filtering operators are called and what they do.
+I recently needed an operator implementing the behavior as described in the previous section. But while there is an operator named `Throttle` in Rx.Net, what it does would be called `debounce` in other languages. See [Background](#Background) for a more thorough explanation.
+
+There is an issue asking for such an operator [Implement a real throttle (not debounce, like the existing throttle)](https://github.com/dotnet/reactive/issues/395), but it has been closed citing that a) Rx.Net was there first and as such their naming shouldn't have to conform to others and b) the maintenance cost of yet an other operator.
+
+And while several "true" throttle variants where sketched in that thread, none included the selector variant and of cause there where no tests. Thus, the decision to create my own implementation including a though test suite.
+
+# Implementation
+
+The implementation of the actual operator is heavily inspired by the rxjs variant. Particularly because I still haven't quite understood how the "native" Rx.Net operator implementations work.
+The unit-tests however are kept in style with Rx.Net and utilize the `TestScheduler`.
+
+# Background
+
+Unfortunately there is significant disparity between the various reactive implementations and what they call their operators. If we just consider the filtering operators, that is,  operators that reduce the number of items emitted involving some time period, they are usually named some variation of `sample`, `throttle` and `debounce`.
+
+reactivex.io only lists and defines two of those operators directly: `sample` and `debounce`.  
+
+> `sample` emits the most recent item emitted by an Observable within periodic time intervals. This operator is sometimes also called `throttleLast`.
+![marble-sample](https://reactivex.io/documentation/operators/images/sample.png){height=250}
+
+> `debounce` only emits an item from an Observable if a particular timespan has passed without it emitting another item. This operator is sometimes also called `throttleWithTimeout`.
+![marble-debounce](https://reactivex.io/documentation/operators/images/debounce.png){height=250}
 
 
- `sample`, `debounce`, `throttle`, `throttleFirst`, `throttleLast` all have incommon that they reduce the number of items emitted by involing some kind of time period.
+But in addition to that, one the page for `sample`, they also define an operator named `throttleFirst`:
 
-Filter operators
+> `throttleFirst` emits the first item and then skips items until the period elapses.
+![marble-throttlefirst](https://reactivex.io/documentation/operators/images/throttleFirst.png){height=250}
 
-absolute timing
-relative timing
-emitt first in intervall
-emitt last in intervall
 
-![](https://reactivex.io/documentation/operators/images/debounce.png){height=250}
+
+
+In Rx.Net however, the behavior of the `Throttle` operator matches the definition of `debounce` from above, which makes the confusion complete.
+
+---
+
+As a side note: In my opinion, the newer, interactive marble diagrams on reactivex.io do not help the matter, due to the fact that they cannot express time-spans like the older, static once do. But at least those are still available, even if they are hidden in the implementation specific expanders.
+
+
+
+
 
--- src/Rx.ThrottleFirst/ThrottleFirstObservableExtensions.cs
+++ src/Rx.ThrottleFirst/ThrottleFirstObservableExtensions.cs
@@ -7,56 +7,56 @@ namespace Rx.ThrottleFirst;
 public static class ThrottleFirstObservableExtensions
 {
     ///<inheritdoc cref="ThrottleFirst{T}(IObservable{T}, TimeSpan, IScheduler)"/>
-    public static IObservable<T> ThrottleFirst<T>(this IObservable<T> source, TimeSpan duration)
-        => source.ThrottleFirst(duration, Scheduler.Default);
+    public static IObservable<T> ThrottleFirst<T>(this IObservable<T> source, TimeSpan suppressionPeriod)
+        => source.ThrottleFirst(suppressionPeriod, Scheduler.Default);
 
     /// <summary>
     /// Emits the <i>first</i> item from the <paramref name="source"/> and then skips subsequent
-    /// items until the given <paramref name="duration"/> has elapsed. After that, the next source
-    /// item is emitted again and the suppression period starts anew.
+    /// items until the given <paramref name="suppressionPeriod"/> has elapsed. The next source item
+    /// after that is emitted again and the suppression period starts anew.
     /// </summary>
     /// <typeparam name="T">Type of the items in <paramref name="source"/></typeparam>
     /// <param name="source">The source Observable</param>
-    /// <param name="duration">The duration for which subsequent items are skipped</param>
+    /// <param name="suppressionPeriod">The duration for which subsequent items are skipped</param>
     /// <param name="scheduler"></param>
     /// <returns>The throttled source</returns>
     /// <exception cref="ArgumentNullException"></exception>
     /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public static IObservable<T> ThrottleFirst<T>(this IObservable<T> source, TimeSpan duration, IScheduler scheduler)
+    public static IObservable<T> ThrottleFirst<T>(this IObservable<T> source, TimeSpan suppressionPeriod, IScheduler scheduler)
     {
         if (source == null)
             throw new ArgumentNullException(nameof(source));
-        if (duration < TimeSpan.Zero)
-            throw new ArgumentOutOfRangeException(nameof(duration), "must not be negative");
+        if (suppressionPeriod < TimeSpan.Zero)
+            throw new ArgumentOutOfRangeException(nameof(suppressionPeriod), "must not be negative");
         if (scheduler is null)
             throw new ArgumentNullException(nameof(scheduler));
-        return source.ThrottleFirst(item => Observable.Timer(duration, scheduler));
+        return source.ThrottleFirst(item => Observable.Timer(suppressionPeriod, scheduler));
     }
 
     /// <summary>
     /// Emits the <i>first</i> item from the <paramref name="source"/> and then skips subsequent
-    /// items until the Observable provided by <paramref name="durationSelector"/> emits. After
-    /// that, the next source item is emitted again and the suppression period starts anew by
-    /// selection the next duration observable.
+    /// items until the Observable provided by <paramref name="supressionPeriodSelector"/> emits.
+    /// The next source item after that is emitted again and the suppression period starts anew by
+    /// selection the next suppression period Observable.
     /// </summary>
     /// <typeparam name="T">Type of the items in <paramref name="source"/></typeparam>
     /// <typeparam name="TThrottle">
-    /// Type of the items in the observable produced by <paramref name="durationSelector"/>. The
-    /// exact type is irrelevant for the purpose of the operator.
+    /// Type of the items in the observable produced by <paramref name="supressionPeriodSelector"/>.
+    /// The exact type is irrelevant for the purpose of the operator.
     /// </typeparam>
     /// <param name="source">The source Observable</param>
-    /// <param name="durationSelector">
+    /// <param name="supressionPeriodSelector">
     /// A function that accepts the source item as input and returns an Observable. The first time
     /// that Observable emits, signals the end of the suppression period.
     /// </param>
     /// <returns>The throttled source</returns>
     /// <exception cref="ArgumentNullException"></exception>
-    public static IObservable<T> ThrottleFirst<T, TThrottle>(this IObservable<T> source, Func<T, IObservable<TThrottle>> durationSelector)
+    public static IObservable<T> ThrottleFirst<T, TThrottle>(this IObservable<T> source, Func<T, IObservable<TThrottle>> supressionPeriodSelector)
     {
         if (source == null)
             throw new ArgumentNullException(nameof(source));
-        if (durationSelector is null)
-            throw new ArgumentNullException(nameof(durationSelector));
+        if (supressionPeriodSelector is null)
+            throw new ArgumentNullException(nameof(supressionPeriodSelector));
         return Observable.Create<T>(observer =>
         {
             T currentValue;
@@ -73,7 +73,7 @@ public static class ThrottleFirstObservableExtensions
 
             void StartThrottle(T value)
             {
-                throttled = durationSelector(value).Subscribe(EndThrottling, observer.OnError, CleanupThrottling);
+                throttled = supressionPeriodSelector(value).Subscribe(EndThrottling, observer.OnError, CleanupThrottling);
             }
 
             void EndThrottling(TThrottle throttle)
