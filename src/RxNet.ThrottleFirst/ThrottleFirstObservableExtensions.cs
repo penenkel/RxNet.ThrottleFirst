6a8a35336da23b451e47ed9d1be7cb9e1611bc33
penenkel <7736404+penenkel@users.noreply.github.com>
2022-07-17 10:35:25 +0200

    Cleanup and simplify implementation

--- src/RxNet.ThrottleFirst/ThrottleFirstObservableExtensions.cs
+++ src/RxNet.ThrottleFirst/ThrottleFirstObservableExtensions.cs
@@ -8,7 +8,7 @@ public static class ThrottleFirstObservableExtensions
 {
     ///<inheritdoc cref="ThrottleFirst{T}(IObservable{T}, TimeSpan, IScheduler)"/>
     public static IObservable<T> ThrottleFirst<T>(this IObservable<T> source, TimeSpan suppressionPeriod)
-        => source.ThrottleFirst(suppressionPeriod, Scheduler.Default);
+        => ThrottleFirst(source, suppressionPeriod, Scheduler.Default);
 
     /// <summary>
     /// Emits the <i>first</i> item from the <paramref name="source"/> and then skips subsequent
@@ -24,89 +24,81 @@ public static class ThrottleFirstObservableExtensions
     /// <exception cref="ArgumentOutOfRangeException"></exception>
     public static IObservable<T> ThrottleFirst<T>(this IObservable<T> source, TimeSpan suppressionPeriod, IScheduler scheduler)
     {
-        if (source == null)
-            throw new ArgumentNullException(nameof(source));
         if (suppressionPeriod < TimeSpan.Zero)
-            throw new ArgumentOutOfRangeException(nameof(suppressionPeriod), "must not be negative");
+            throw new ArgumentOutOfRangeException(nameof(suppressionPeriod), "Cannot be negative.");
         if (scheduler is null)
             throw new ArgumentNullException(nameof(scheduler));
-        return source.ThrottleFirst(item => Observable.Timer(suppressionPeriod, scheduler));
+        return ThrottleFirst(source, item => Observable.Timer(suppressionPeriod, scheduler));
     }
 
     /// <summary>
     /// Emits the <i>first</i> item from the <paramref name="source"/> and then skips subsequent
-    /// items until the Observable provided by <paramref name="supressionPeriodSelector"/> emits.
+    /// items until the Observable provided by <paramref name="suppressionPeriodSelector"/> emits.
     /// The next source item after that is emitted again and the suppression period starts anew by
     /// selection the next suppression period Observable.
     /// </summary>
     /// <typeparam name="T">Type of the items in <paramref name="source"/></typeparam>
     /// <typeparam name="TThrottle">
-    /// Type of the items in the observable produced by <paramref name="supressionPeriodSelector"/>.
-    /// The exact type is irrelevant for the purpose of the operator.
+    /// Type of the items in the observable produced by <paramref
+    /// name="suppressionPeriodSelector"/>. The exact type is irrelevant for the purpose of the operator.
     /// </typeparam>
     /// <param name="source">The source Observable</param>
-    /// <param name="supressionPeriodSelector">
+    /// <param name="suppressionPeriodSelector">
     /// A function that accepts the source item as input and returns an Observable. The first time
     /// that Observable emits, signals the end of the suppression period.
     /// </param>
     /// <returns>The throttled source</returns>
     /// <exception cref="ArgumentNullException"></exception>
-    public static IObservable<T> ThrottleFirst<T, TThrottle>(this IObservable<T> source, Func<T, IObservable<TThrottle>> supressionPeriodSelector)
+    public static IObservable<T> ThrottleFirst<T, TThrottle>(this IObservable<T> source, Func<T, IObservable<TThrottle>> suppressionPeriodSelector)
     {
-        if (source == null)
+        if (source is null)
             throw new ArgumentNullException(nameof(source));
-        if (supressionPeriodSelector is null)
-            throw new ArgumentNullException(nameof(supressionPeriodSelector));
+        if (suppressionPeriodSelector is null)
+            throw new ArgumentNullException(nameof(suppressionPeriodSelector));
         return Observable.Create<T>(observer =>
         {
             T currentValue;
-            bool isComplete = false;
-            IDisposable? throttled = null;
+            IDisposable? throttling = null;
 
             void Send(T value)
             {
                 currentValue = value;
                 observer.OnNext(currentValue);
-                if (!isComplete)
-                    StartThrottle(value);
+                StartThrottling(value);
             }
 
-            void StartThrottle(T value)
+            void StartThrottling(T value)
             {
-                throttled = supressionPeriodSelector(value).Subscribe(EndThrottling, observer.OnError, CleanupThrottling);
+                throttling = suppressionPeriodSelector(value).Subscribe(
+                    _ => EndThrottling(),
+                    observer.OnError,
+                    EndThrottling);
             }
 
-            void EndThrottling(TThrottle throttle)
+            void EndThrottling()
             {
-                throttled?.Dispose();
-                throttled = null;
-            };
-
-            void CleanupThrottling()
-            {
-                throttled = null;
-                if (isComplete)
-                    observer.OnCompleted();
+                throttling?.Dispose();
+                throttling = null;
             };
 
             var subscription = source.Subscribe(
                 value =>
                 {
-                    if (throttled is null)
+                    if (throttling is null)
                         Send(value);
                 },
                 observer.OnError,
-                () =>
-                {
-                    isComplete = true;
-                    observer.OnCompleted();
-                }
+                observer.OnCompleted
             );
+
+            // NOTE: If the outer observable completes or ends with an error, the correct disposal
+            // of the inner observable depends on the fact that the outer subscription is
+            // automatically unsubscripted. Within RxNet this should be the case for all well formed observables.
 
             return Disposable.Create(() =>
             {
                 subscription.Dispose();
-                throttled?.Dispose();
+                throttling?.Dispose();
             });
         });
     }
